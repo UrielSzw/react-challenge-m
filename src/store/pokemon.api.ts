134266f2e13dbe0cd7d1df1dd4d4c0c598269ce7
UrielSzw <urielszw@gmail.com>
2024-12-09 16:37:07 -0300

    Fix

--- src/store/pokemon.api.ts
+++ src/store/pokemon.api.ts
@@ -4,7 +4,7 @@ import { PokemonListResponse } from "../types/pokemon.types";
 export const pokemonApi = createApi({
   reducerPath: "pokemonApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.NEXT_PUBLIC_POKEMON_API_BASE_URL,
+    baseUrl: "https://pokeapi.co/api/v2/",
   }),
   endpoints: (builder) => ({
     getPokemons: builder.query<
